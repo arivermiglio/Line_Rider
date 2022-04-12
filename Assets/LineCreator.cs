508d6d2180ebc1f00139e63c7c278b0bf83a1c29
arivermiglio <aria.vermiglio@mail.utoronto.ca>
2022-04-11 22:13:30 -0400

    with line smoothing

--- Assets/LineCreator.cs
+++ Assets/LineCreator.cs
@@ -14,5 +14,20 @@ public class LineCreator : MonoBehaviour
             GameObject lineGO = Instantiate(linePrefab);
             activeLine = lineGO.GetComponent<Line>();
         }
+
+        // the user finishes drawing a line
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        // if the user is still drawing the line
+        if (activeLine != null)
+        {
+            // update the line according to current mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+
     }
 }
