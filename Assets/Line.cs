8bddacbdcde0683be47120bac8d1ed2f4719f5ce
arivermiglio <aria.vermiglio@mail.utoronto.ca>
2022-04-11 21:32:03 -0400

    18:23

---
+++ Assets/Line.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+
+    List<Vector2> points;
+
+    public void UpdateLine(Vector2 mousePos)
+    {
+        if (points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(mousePos);
+            return;
+        }
+        // **check** if the mouse has moved enough for us to insert new point
+        // points.last() is the same as points[points.Count - 1]
+        if (Vector2.Distance(points.Last(), mousePos) > .1f)
+        {
+            SetPoint(mousePos);
+        }
+
+
+
+        // if it has, insert point at mouse position
+
+    }
+    void SetPoint(Vector2 point)
+    {
+        // point is mouse position
+        // add point to the points list
+        points.Add(point);
+
+        // add point to the line renderer
+        // 1. set the num of points on the line renderer = length of points list
+        lineRenderer.positionCount = points.Count;
+        // 2. add the point to the last index
+        lineRenderer.SetPosition(points.Count - 1, point);
+        edgeCollider.points = points.ToArray();
+
+
+    }
+}
